db0c900211160782db66eaed29bcf1aa32b146dd
kiplongu <rodgersbiwott2016@gmail.com>
2023-09-14 20:50:11 +0300

    added terrraform vars

--- asg.tf
+++ asg.tf
@@ -1,10 +1,10 @@
 resource "aws_autoscaling_group" "web-server" {
   name     = "nginx-asg"
-  max_size = 5
+  max_size = 2
   min_size = 2
   #health_check_grace_period = 300
   #health_check_type         = "ELB"
-  desired_capacity = 4
+  desired_capacity = 2
   force_delete     = true
   #placement_group           = aws_placement_group.test.id
   launch_configuration = aws_launch_configuration.web-server-launch-config.name
